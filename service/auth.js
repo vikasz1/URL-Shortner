2467309ca5fd568190a2b3aae7f46a61a0871c20
vikasz1 <mauryvikas12345@gmail.com>
2023-09-10 13:36:48 +0530

    stateless authentication implemented

--- service/auth.js
+++ service/auth.js
@@ -1,13 +1,19 @@
-const sessionIdUserMap = new Map()
+const jwt = require('jsonwebtoken')
+const secret = "Vikas12345"
 
-function setUser(id,user){
-    sessionIdUserMap.set(id,user)
+function setUser(user) {
+
+    return jwt.sign({
+        _id: user._id,
+        email: user.email
+    }, secret)
 }
 
-function getUser(id){
-    return sessionIdUserMap.get(id)
+function getUser(token) {
+    if (!token) return null
+    return jwt.verify(token, secret)
 }
 
-module.exports= {
-    getUser,setUser
+module.exports = {
+    getUser, setUser
 }
