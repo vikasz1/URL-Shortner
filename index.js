d5d74d7baf49f8da4676a207511067cf9d440567
vikasz1 <mauryvikas12345@gmail.com>
2023-10-30 16:12:01 +0530

    cloud database added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path")
+require('dotenv').config()
 const cookieParser  = require('cookie-parser')
 const {restrictToLoggedUserOnly} = require('./middlewares/auth')
+const mongo_url= process.env.MONGO_URI
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
@@ -18,11 +20,11 @@ const userRoute =  require('./routers/user')
 
 app.use("/url",restrictToLoggedUserOnly, urlRoute);
 app.use('/',staticRoute)
-app.use('/user',userRoute)
+app.use('/user',userRoute) 
 
 const PORT = 8001;
 
-startConnection("mongodb://localhost:27017/url-shortner")
+startConnection(mongo_url)
 app.set("view engine", "ejs")
 app.set('views', path.resolve('./views'))
 
